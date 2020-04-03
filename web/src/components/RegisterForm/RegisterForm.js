81fa1239b9a5a4c7cc113834b2c779eab95ae7f6
David Isakson <ikeman32@yahoo.com>
2020-04-03 08:16:41 -0700

    registration from fields and labels

--- web/src/components/RegisterForm/RegisterForm.js
+++ web/src/components/RegisterForm/RegisterForm.js
@@ -1,8 +1,24 @@
+import {
+  Form,
+  TextField,
+  Label,
+  Submit,
+  FieldError,
+  FormError,
+} from '@redwoodjs/web'
+import { useForm } from 'react-hook-form'
 const RegisterForm = () => {
   return (
     <div>
-      <h2>{'RegisterForm'}</h2>
-      <p>{'Find me in ./web/src/components/RegisterForm/RegisterForm.js'}</p>
+      <Form>
+        <Label htmlFor="username"></Label>
+        <TextField name="username" validation={{ required: true }} />
+        <Label htmlFor="password1"></Label>
+        <TextField name="password1" type="password" />
+        <Label htmlFor="password2"></Label>
+        <TextField name="password2" type="password" />
+        <Submit>Register</Submit>
+      </Form>
     </div>
   )
 }
